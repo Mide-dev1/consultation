cfaca492c6584790fcf1f8b8bf4b619a729cbe74
Mide-dev1 <olamidejayyy@gmail.com>
2025-04-03 16:13:16 +0100

    Delete backend/routes directory

--- backend/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentController = require('../controllers/paymentController');
-
-router.get('/verify', paymentController.verifyPayment);
-router.post('/initialize', paymentController.initializePayment);
-
-module.exports = router;
